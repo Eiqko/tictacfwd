dd11582dfe86202959f3b9b06bf485361ba06966
Eiqko <sarandaeiko@gmail.com>
2020-06-22 23:57:19 +0700

    committed

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+
+        <a href="http://github.com/Eiqko" target="_blank" rel="noreferrer noopener">
+          Eiqko
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
