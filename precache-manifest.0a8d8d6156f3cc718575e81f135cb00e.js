e0b6f64316afc57316bb6b7958c559e3102204d9
Eiqko <sarandaeiko@gmail.com>
2020-06-23 00:03:19 +0700

    Updates

---
+++ precache-manifest.0a8d8d6156f3cc718575e81f135cb00e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce9448a46ee427424e699f3dab9645c",
+    "url": "/tictacfwd/index.html"
+  },
+  {
+    "revision": "14a419d05c5f5e3488f0",
+    "url": "/tictacfwd/static/css/main.cab3b057.chunk.css"
+  },
+  {
+    "revision": "330cac20a467e2af9512",
+    "url": "/tictacfwd/static/js/2.0135eb43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictacfwd/static/js/2.0135eb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a419d05c5f5e3488f0",
+    "url": "/tictacfwd/static/js/main.0e9fba96.chunk.js"
+  },
+  {
+    "revision": "fdb36cf89789c7452a0d",
+    "url": "/tictacfwd/static/js/runtime-main.e882ce57.js"
+  },
+  {
+    "revision": "f1624a85c970e64237b5e4bf4b87c39a",
+    "url": "/tictacfwd/static/media/bg.f1624a85.jpg"
+  }
+]);
